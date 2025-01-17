c4f81fd1725743a8f35952d6a2b482c0be270a00
calexanderberg <carlalexanderberg@gmail.com>
2025-01-17 12:39:15 +0100

    Fixed: examples, hotfix we need to make it better

--- examples/usage.rs
+++ examples/usage.rs
@@ -5,17 +5,17 @@ fn main() {
     let b = Value::from(2.0);
 
     let mut c = &a + &b;
-    let mut d = &a * &b + b.pow(3.0);
+    let mut d = &a * &b + b.pow(&Value::from(3.0));
 
-    c += &c + 1.0;
-    c += 1.0 + &c + (-&a);
-    d += &d * 2.0 + (&b + &a).relu();
-    d += 3.0 * d + (&b - &a).relu();
+    c = &c + &Value::from(1.0);
+    c = &Value::from(1.0) + &c + (-&a);
+    d = &d + &(&d * &Value::from(2.0)) + (&b + &a).relu();
+    d = &d + &(&Value::from(3.0) * &d) + (&b - &a).relu();
 
     let e = &c - &d;
-    let f = e.pow(2.0);
-    let mut g = &f / 2.0;
-    g += 10.0 / &f;
+    let f = e.pow(&Value::from(2.0));
+    let mut g = &f / &Value::from(2.0);
+    g = g + &Value::from(10.0) / &f;
 
     println!("{:.4}", g.borrow().data); // prints 24.7041, the outcome of this forward pass
     g.backward();
