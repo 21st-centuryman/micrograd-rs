47cb8928ad9429868995b221d6d2cb9984ec290a
calexanderberg <carlalexanderberg@gmail.com>
2025-07-10 19:52:26 +0200

    Del: neurons

--- src/nn.rs
+++ src/nn.rs
@@ -1,83 +1,48 @@
 use crate::engine::Value;
 use rand::Rng;
 
-pub struct Neuron<const P: usize> {
-    w: [Value; P],
-    b: Value,
-    nonlin: bool,
-}
-
-impl<const P: usize> Neuron<P> {
-    pub fn new(nonlin: bool) -> Self {
-        Neuron {
-            w: std::array::from_fn(|_| Value::from(rand::thread_rng().gen_range(-1.0..=1.0))),
-            b: Value::from(0.0), // Removing the bias
-            nonlin,
-        }
-    }
-
-    pub fn forward(&self, x: &[Value; P]) -> Value {
-        let out = match P {
-            1 => (&self.w[0] * &x[0]) + self.b.clone(),
-            2 => (&self.w[0] * &x[0] + &self.w[1] * &x[1]) + self.b.clone(),
-            _ => {
-                let mut sum = Value::from(0.0);
-                for (wi, xi) in self.w.iter().zip(x.iter()) {
-                    sum = sum + (wi * xi);
-                }
-                sum + self.b.clone()
-            }
-        };
-        return if self.nonlin { out.relu() } else { out };
-    }
-
-    pub fn parameters(&self) -> impl Iterator<Item = &Value> {
-        self.w.iter().chain(std::iter::once(&self.b))
-    }
-
-    //pub fn parameters(&self) -> [Value; P + 1] {
-    //    std::array::from_fn(|i| self.w.get(i).cloned().unwrap_or_else(|| self.b.clone()))
-    //}
-}
-
-impl<const N: usize> std::fmt::Debug for Neuron<N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}Neuron({})", if self.nonlin { "ReLU" } else { "Linear" }, N)
-    }
-}
+//impl<const N: usize> std::fmt::Debug for Neuron<N> {
+//    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//        write!(f, "{}Neuron({})", if self.nonlin { "ReLU" } else { "Linear" }, N)
+//    }
+//}
 
 pub struct Layer<const P: usize, const N: usize> {
-    neurons: [Neuron<P>; N],
+    w: [[Value; P]; N],
+    b: [Value; N],
+    nonlin: bool,
 }
 
 impl<const P: usize, const N: usize> Layer<P, N> {
     pub fn new(nonlin: bool) -> Layer<P, N> {
         Self {
-            neurons: std::array::from_fn(|_| Neuron::new(nonlin)),
+            w: std::array::from_fn(|_| std::array::from_fn(|_| Value::from(rand::thread_rng().gen_range(-1.0..=1.0)))),
+            b: std::array::from_fn(|_| Value::from(0.0)),
+            nonlin,
         }
     }
 
     pub fn forward(&self, x: &[Value; P]) -> [Value; N] {
-        std::array::from_fn(|i| self.neurons[i].forward(x))
+        std::array::from_fn(|i| {
+            let out = self.w[i].iter().zip(x.iter()).map(|(w, x)| w * x).reduce(|a, b| a + b).unwrap() + self.b[i].clone();
+            return if self.nonlin { out.relu() } else { out };
+        })
     }
 
     pub fn parameters(&self) -> impl Iterator<Item = &Value> {
-        self.neurons.iter().flat_map(|n| n.parameters())
+        self.w.iter().zip(self.b.iter()).flat_map(|(ws, b)| ws.iter().chain(std::iter::once(b)))
     }
+}
 
-    //pub fn parameters(&self) -> [Value; N * (P + 1)] {
-    //    std::array::from_fn(|i| self.neurons[i / (P + 1)].parameters()[i % (P + 1)].clone())
-    //}
-}
-impl<const P: usize, const N: usize> std::fmt::Debug for Layer<P, N> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "Layer of [{:?}]",
-            self.neurons.iter().map(|n| format!("{:?}", n)).collect::<Vec<_>>().join(", ")
-        )
-    }
-}
+//impl<const P: usize, const N: usize> std::fmt::Debug for Layer<P, N> {
+//    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//        write!(
+//            f,
+//            "Layer of [{:?}]",
+//            self.neurons.iter().map(|n| format!("{:?}", n)).collect::<Vec<_>>().join(", ")
+//        )
+//    }
+//}
 
 pub struct MLP<const N1: usize, const N2: usize, const N3: usize, const N4: usize> {
     l1: Layer<N1, N2>,
@@ -114,8 +79,8 @@ impl<const N1: usize, const N2: usize, const N3: usize, const N4: usize> MLP<N1,
     //}
 }
 
-impl<const N1: usize, const N2: usize, const N3: usize, const N4: usize> std::fmt::Debug for MLP<N1, N2, N3, N4> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "MLP of [{:?}, {:?}, {:?}]", self.l1, self.l2, self.l3)
-    }
-}
+//impl<const N1: usize, const N2: usize, const N3: usize, const N4: usize> std::fmt::Debug for MLP<N1, N2, N3, N4> {
+//    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//        write!(f, "MLP of [{:?}, {:?}, {:?}]", self.l1, self.l2, self.l3)
+//    }
+//}
